c415a6266cd8955cbdfeb88e105b8c26e24c7385
Aziza Sadritdinova <asadritd@c2s19d127.42wolfsburg.de>
2022-09-15 20:14:12 +0200

    utils strdup&calloc

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: asadritd <asadritd@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 13:16:57 by asadritd          #+#    #+#             */
-/*   Updated: 2022/09/15 19:21:22 by asadritd         ###   ########.fr       */
+/*   Updated: 2022/09/15 20:02:16 by asadritd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ char	wh_remain(char *remain, char **line)
 	
 	ptr_n = NULL;
 	if(remain)	
-		if((ptr_n = ft_strchr(remain, '\n')))
+		if((ptr_n = gnl_strchr(remain, '\n')))
 		{
 			*ptr_n = '\0';
-			*line = ft_strdup(remain);
+			*line = gnl_strdup(remain);
 			ft_strcpy(remain, ++ptr_n);
 		}
 		else
 		{
-			*line = ft_strdup(remain);
+			*line = gnl_strdup(remain);
 			strclr(remain);
 		}
 	else
@@ -47,11 +47,11 @@ char	*get_next_line(int fd)
 	while (!ptr_n && (bt_read = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[bt_read] = '\0';
-		if((ptr_n = ft_strchr(buf, '\n')))
+		if((ptr_n = gnl_strchr(buf, '\n')))
 		{
 			*ptr_n = '\0';
 			ptr_n++;
-			remain = ft_strdup(ptr_n);
+			remain = gnl_strdup(ptr_n);
 		}
 		keep = *line;
 		*line = ft_strjoin(*line, buf);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,55 @@
 /*   By: asadritd <asadritd@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 13:16:52 by asadritd          #+#    #+#             */
-/*   Updated: 2022/09/13 16:21:29 by asadritd         ###   ########.fr       */
+/*   Updated: 2022/09/15 20:13:32 by asadritd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*gnl_strdup(const char *str1)
+{
+	int i;
+	int len; 
+	char *str2;
+
+	len = 0;
+	while (str1[len])
+		len++;
+	str2 = (char *)malloc(sizeof(char) * (len + 1));
+	if (!(str2))
+	{
+		return(NULL);
+	}
+	i = 0;
+	while (str1[i])
+	{
+		str2[i] = str1[i];
+		i++;		
+	}
+	str2[i] = '\0';
+	return (str2);
+}
+
+char	*gnl_strchr()
+
+char	*gnl_strjoin()
+
+char	*gnl_strcpy()
+
+char	*gnl_calloc(int count)
+{
+	char *str;
+	int it;
+	
+	it = 0;
+	str = malloc (count * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (it < count)
+		str[it++] = '\0';
+	return (str);
+}
+
+strclr 
 
