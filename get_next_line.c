62eedacbf522fd778957be96e74ff7d8ce853e1a
Aziza Sadritdinova <asadritd@c2s13d71.42wolfsburg.de>
2022-09-21 19:28:36 +0200

    finalcorrections, divided into more functions for norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,17 @@
 /*   By: asadritd <asadritd@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 13:16:57 by asadritd          #+#    #+#             */
-/*   Updated: 2022/09/20 17:08:45 by asadritd         ###   ########.fr       */
+/*   Updated: 2022/09/21 19:23:35 by asadritd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*gnl_strcpy(char *dest, const char *src)
+struct	structVar
 {
-	int	i;
-
-	i = 0;
-	while (src[i] != 0)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
+	int bt_read;
+	char		buf[BUFF_SIZE + 1];
+} var;
 
 char	*wh_remain(char *remain, char **line)
 {
@@ -37,7 +29,8 @@ char	*wh_remain(char *remain, char **line)
 		{
 			*ptr_n = '\0';
 			*line = gnl_strdup(remain);
-			gnl_strcpy(remain, ++ptr_n);
+			++ptr_n;
+			gnl_strlcpy(remain, ptr_n, gnl_strlen(ptr_n) + 1);
 		}
 		else
 		{
@@ -46,80 +39,75 @@ char	*wh_remain(char *remain, char **line)
 		}
 	}
 	else
-		*line = gnl_calloc(sizeof(char));
+		*line = malloc(((int)sizeof(char)) * sizeof(char));
 	return(ptr_n);
 }
 
-char	*gnl_calloc(int count)
+char	*x_cases(char **line, char **keep, char *remain)
 {
-	char *str;
-	int it;
-	
-	it = 0;
-	str = malloc (count * sizeof(char));
-	if (!str)
+	if (var.bt_read <= BUFF_SIZE && **line)
+	{
+		var.buf[0] = '\0';
+		return (*line);
+	}
+	else if (var.bt_read <= 0)
+	{
+		if (remain)
+			free(remain);
+	 	free(*line);
+		*line = NULL;
 		return (NULL);
-	while (it < count)
-		str[it++] = '\0';
-	return (str);
+	}
+	else
+	{
+		*keep = gnl_strjoin(*line, "\n");
+		free(*line);
+		return (*keep);
+	}
+}
+
+char	*func(char **ptr_n, char **remain, char **line, char **keep)
+{
+	**ptr_n = '\0';
+	(*ptr_n)++;
+	if (*remain)
+		free(*remain);
+	*remain = gnl_strdup(*ptr_n);
+	*keep = *line;
+	*line = gnl_strjoin(*line, var.buf);
+	free(*keep);
+	*keep = gnl_strjoin(*line, "\n");
+	free(*line);
+	return (*keep);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*remain;
 	char 		*line;
-	char		buf[BUFF_SIZE + 1];
-	int			bt_read;
 	char		*ptr_n;
 	char		*keep;
 
 	if (fd < 0)
 		return ((char *)0);
 	ptr_n = wh_remain(remain, &line);
-	while (!ptr_n && (bt_read = read(fd, buf, BUFF_SIZE)))
+	while (!ptr_n && (var.bt_read = read(fd, var.buf, BUFF_SIZE)))
 	{
-		if (bt_read < 0)
+		if (var.bt_read < 0)
 		{
 			free(line);
 			return (NULL);
 		}
-		buf[bt_read] = '\0';
-		if((ptr_n = gnl_strchr(buf, '\n')))
-		{
-			*ptr_n = '\0';
-			ptr_n++;
-			if (remain) free(remain);
-			remain = gnl_strdup(ptr_n);
-			keep = line;
-			line = gnl_strjoin(line, buf);
-			free(keep);
-			keep = gnl_strjoin(line, "\n");
-			free(line);
-			return (keep);
-		}
+		var.buf[var.bt_read] = '\0';
+		if((ptr_n = gnl_strchr(var.buf, '\n')))
+			return (func(&ptr_n, &remain, &line, &keep));
 		keep = line;
-		line = gnl_strjoin(line, buf);
+		line = gnl_strjoin(line, var.buf);
 		free(keep);
 	}
-	if (bt_read <= BUFF_SIZE && *line)
-	{
-		buf[0] = '\0';
-		return (line);
-	}
-	else if (bt_read <= 0)
-	{
-		if (remain) free(remain);
-	 	free(line);
-		line = NULL;
-		return (NULL);
-	}
-	else // bt_read <= BUFF_SIZE && !(*line)
-	{
-		keep = gnl_strjoin(line, "\n");
-		free(line);
-		return (keep);
-	}
+	return (x_cases(&line, &keep, remain));
 }
+
 
 // int main(void)
 // {
